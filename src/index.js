e9b75f719b6015bd4435d5f6cb0fd300e7b5bc80
Subhasree Bose <subhasree.bose2014@gmail.com>
2020-06-02 15:59:11 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <HashRouter basename="/">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
