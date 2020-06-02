9576f5ae142a4b33e9b313bc9155f685e785e346
Subhasree Bose <subhasree.bose2014@gmail.com>
2020-06-02 16:05:40 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import News from "./News";
 import Details from "./Details";
 import Open from "./open";
