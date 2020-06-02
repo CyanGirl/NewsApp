2f9333c527ca1c445d8c0445284e75d8557ba106
Subhasree Bose <subhasree.bose2014@gmail.com>
2020-06-02 14:23:48 +0530

    Updates

---
+++ precache-manifest.a1ba22f3fc36f9806d320d0dff66abf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f099e0611ac0b6408f760baca97d133",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "bbaa7a07c250a66d9a05",
+    "url": "/NewsApp/static/css/main.bbc4096c.chunk.css"
+  },
+  {
+    "revision": "b7b6b79ebb4a824aabcb",
+    "url": "/NewsApp/static/js/2.d9c988c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewsApp/static/js/2.d9c988c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbaa7a07c250a66d9a05",
+    "url": "/NewsApp/static/js/main.a45db98e.chunk.js"
+  },
+  {
+    "revision": "155841f397d10a5e8a8a",
+    "url": "/NewsApp/static/js/runtime-main.a24d4dec.js"
+  },
+  {
+    "revision": "b406439dc5f922a1a6bafd55f0aa8920",
+    "url": "/NewsApp/static/media/image2.b406439d.jpg"
+  }
+]);
